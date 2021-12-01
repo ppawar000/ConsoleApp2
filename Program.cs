eb5d7e8d3e11bd109209693c72f9413bcc9c6f25
ppawar000 <95282332+ppawar000@users.noreply.github.com>
2021-12-01 17:38:18 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ namespace ConsoleApp2
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Explaining git");
+            console.WriteLine("hekkkooooooo");
         }
     }
 }
